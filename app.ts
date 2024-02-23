0e1c13b85e341b0f6830c91c746761b7c0ab0d63
david123456858 <juandavidperaltafuentes@gmail.com>
2024-02-22 21:18:25 -0500

    conexion lista

--- app.ts
+++ app.ts
@@ -1,10 +1,15 @@
 import express, { Request, Response } from 'express';
 import morgan from 'morgan'
+
 import route from './src/router/userOperario';
+import { pool } from './src/db/db';
 
 const app = express()
 
+
+
 const PORT = process.env.PORT ?? 3000
+
 
 app.use(express.json())
 app.use(morgan('dev'))
---
+++ src/db/db.ts
@@ -0,0 +1,15 @@
+import { Pool } from "pg"
+import {config} from 'dotenv'
+
+config()
+
+export const pool = new Pool({
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: 5432,
+})
+
+console.log(pool)
+export default pool
