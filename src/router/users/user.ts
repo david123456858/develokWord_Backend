a1ca175d62541dd232a541d08989d17c17c269e4
david123456858 <juandavidperaltafuentes@gmail.com>
2024-05-15 18:06:56 -0500

    cositas que nise

--- src/router/users/user.ts
+++ src/router/users/user.ts
@@ -1,11 +1,8 @@
-import { createToken, createUser, getAllUser, updateUserTeams, verifyUser, updateUser,changePassWord, getInfo } from "../../controller/user/user"
+import { createToken, createUser, getAllUser, updateUserTeams, verifyUser, updateUser, changePassWord, getInfo } from "../../controller/user/user"
 import { Router } from "express";
-//import { checkJwt } from "../../middleware/jwt/userJwt";
-
-
 const routeBase = '/api/v1/user'
 
-const routeUser = Router() 
+const routeUser = Router()
 
 routeUser.get(`${routeBase}/getAtri`, getInfo)
 routeUser.post(`${routeBase}/createUser`, createUser)//crear usuario
@@ -14,6 +11,6 @@ routeUser.get(`${routeBase}/create`, createToken)//ESTO NO TE IMPORTA SAPO
 routeUser.get(`${routeBase}/getUsers`, getAllUser)//
 routeUser.put(`${routeBase}/updateTeams`, updateUserTeams)
 routeUser.put(`${routeBase}/updateUser/:id`, updateUser)
-routeUser.put(`${routeBase}/newPass/:id`,changePassWord)
+routeUser.put(`${routeBase}/newPass/:id`, changePassWord)
 
-export default routeUser 
+export default routeUser
