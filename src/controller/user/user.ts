1b1db3e7547a9ac4288bd7e173f21e382ce90ab6
david123456858 <juandavidperaltafuentes@gmail.com>
2024-05-27 20:45:55 -0500

    update teams and password

--- src/controller/user/user.ts
+++ src/controller/user/user.ts
@@ -8,8 +8,6 @@ import { User } from '../../entity/user'
 import { tokenSing } from '../../helpers/tokensHelpers'
 // import { employes } from '../../entity/employes'
 
-// verificar usuario
-
 export const verifyUser = async (req: Request, res: Response): Promise<void> => {
   try {
     const { correo, contra } = req.body
@@ -46,7 +44,6 @@ export const verifyUser = async (req: Request, res: Response): Promise<void> => 
     console.log(error)
   }
 }
-
 export const createUser = async (req: Request, res: Response): Promise<void> => {
   try {
     const { idUsuario, nombre1, nombre2, apellido1, apellido2, correo, contra, rol, estado } = req.body
@@ -77,7 +74,6 @@ export const createUser = async (req: Request, res: Response): Promise<void> => 
     res.status(505).json({ info: 'Internal error server' })
   }
 }
-
 export const getAllUser = async (req: Request, res: Response): Promise<void> => {
   try {
     const users = await employes.find({
@@ -93,70 +89,35 @@ export const getAllUser = async (req: Request, res: Response): Promise<void> => 
     res.status(505).json({ info: 'Internal error server' })
   }
 }
-
 export const updateUserTeams = async (req: Request, res: Response): Promise<void> => {
-  // try {
-  //   const { idUsuario, idEquipo } = req.body
-  //   console.log(idUsuario, idEquipo)
-  //   if (idUsuario === null || idEquipo === null) {
-  //     res.status(422).json({
-  //       detail: {
-  //         info: 'Unprocessable Content',
-  //         message: 'No se han enviado todos los datos necesarios'
-  //       }
-  //     })
-  //     return
-  //   }
-  //   const queryDefault: string = `UPDATE public.usuarios
-  //       SET idEquipo=$2 WHERE usuarios.idUsuario =$1`
-
-  //   res.status(200).json({ data: 'Usuario asignado a un grupos' })
-  // } catch (error) {
-  //   console.log(error)
-  //   res.status(505).json({ info: 'Internal error server' })
-  // }
+  try {
+    const { idUsuario, idEquipo } = req.body
+    if (idUsuario === null || idEquipo === null) {
+      res.status(422).json({ message: 'No se han enviado todos los datos necesarios' })
+      return
+    }
+    const user = await employes.findOneBy({ id_usuario: idUsuario })
+    if (user === null) {
+      res.status(404).json({ message: 'User does dont exist' })
+      return
+    }
+    user.idEquipo = idEquipo
+    await user.save()
+    res.status(200).json({ data: 'Usuario asignado a un grupos' })
+  } catch (error) {
+    console.log(error)
+    res.status(505).json({ info: 'Internal error server' })
+  }
 }
-
 export const updateUser = async (req: Request, res: Response): Promise<void> => {
-  // try {
-  //   const idUsuario = (req.params.id)
-  //   const { nombre1, nombre2, apellido1, apellido2 } = req.body
-  //   let quer: string = 'UPDATE public.usuarios SET '
-  //   const updateQuery = []
-  //   let index = 1
-  //   if (nombre1) {
-  //     quer += `nombre1=$${index} ,`
-  //     updateQuery.push(nombre1)
-  //     index++
-  //   }
-  //   if (nombre2) {
-  //     quer += `nombre2=$${index} ,`
-  //     updateQuery.push(nombre2)
-  //     index++
-  //   }
-  //   if (apellido1) {
-  //     quer += `apellido1=$${index} ,`
-  //     updateQuery.push(apellido1)
-  //     index++
-  //   }
-  //   if (apellido2) {
-  //     quer += `apellido2=$${index} ,`
-  //     updateQuery.push(apellido2)
-  //     index++
-  //   }
-  //   quer = quer.slice(0, -1)
-  //   quer += `WHERE idUsuario = '${idUsuario}'`
-  //   console.log(quer)
-
-  //   console.log(updateQuery)
-  //   await connect.query(quer, updateQuery)
-  //   res.status(202).json({ data: 'update user succeFully' })
-  // } catch (error) {
-  //   console.log(error)
-  //   res.status(505).json({ info: 'Internal error server' })
-  // }
+  try {
+    const { nombre1, nombre2, apellido1, apellido2 } = req.body
+    res.status(202).json({ data: 'update user succeFully' })
+  } catch (error) {
+    console.log(error)
+    res.status(505).json({ info: 'Internal error server' })
+  }
 }
-
 export const changePassWord = async (req: Request, res: Response): Promise<void> => {
   try {
     const { contra } = req.body
@@ -174,7 +135,6 @@ export const changePassWord = async (req: Request, res: Response): Promise<void>
     res.status(505).json({ info: 'Internal error server' })
   }
 }
-
 export const getInfo = async (req: Request, res: Response): Promise<void> => {
   try {
     const responseR = await estados.find()
