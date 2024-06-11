410afa2e8c29c9fbdc259b17bbed391f7933c34f
david123456858 <juandavidperaltafuentes@gmail.com>
2024-06-11 15:16:22 -0500

    update ruta

--- src/router/users/user.ts
+++ src/router/users/user.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 
-import { createUser, getAllUser, updateUserTeams, verifyUser, changePassWord, getInfo } from '../../controller/user/user'
+import { createUser, getAllUser, updateUserTeams, verifyUser, changePassWord, getInfo, updateUser } from '../../controller/user/user'
 import { createToken } from '../../controller/tokens/tokens'
 const routeBase = '/api/v1/user'
 
@@ -13,5 +13,6 @@ routeUser.get(`${routeBase}/create`, createToken)// ESTO NO TE IMPORTA SAPO
 routeUser.get(`${routeBase}/getUsers`, getAllUser)// trae a todo los mmguevo
 routeUser.put(`${routeBase}/updateTeams`, updateUserTeams)
 routeUser.put(`${routeBase}/newPass/:id`, changePassWord)
+routeUser.put(`${routeBase}/updateUser`, updateUser)
 
 export default routeUser
